a5a03e965dcded62645c24c3341a28edba94a6fd
Anthony F <aferragano@gmail.com>
2015-08-06 17:08:16 -0700

    works on mobile ... ish

--- client/views/page.js
+++ client/views/page.js
@@ -7,78 +7,84 @@ if (Meteor.isClient) {
 
   Template.page.events({
     //map view only
-    'click #map-icon-btn': function () {
+    'touchstart #map-icon-btn': function (event, template) {
+      document.getElementById('muro-welcome').style.display="none"
+      document.getElementById('main-nav').style.display="none"
+      document.getElementById('peek').style.display="block"
+      document.getElementById('logo-corner').style.display="block"
+    },
+    'touchstart #muro-welcome': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('main-nav').style.display="none"
       document.getElementById('peek').style.display="block"
       document.getElementById('logo-corner').style.display="block"
     },
     //show search field 
-    'click #find-icon-btn': function () {
+    'touchstart #find-icon-btn': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('main-nav').style.display="none"
       document.getElementById('peek-search').style.display="block"
       document.getElementById('logo-corner').style.display="block"
     },
     //bye bye search field
-    'click #x-circle': function () {
+    'touchstart #x-circle': function (event, template) {
       document.getElementById('peek-search').style.display="none" 
       document.getElementById('peek').style.display="block" 
       document.getElementById('logo-corner').style.display="block"     
     },
     //show full search view
-    'click #search': function () {
+    'touchstart #search': function (event, template) {
       document.getElementById('peek-search').style.display="none" 
       document.getElementById('peek').style.display="none" 
       document.getElementById('logo-corner').style.display="none"
       document.getElementById('full-search').style.display="block"
     },
-    'click #x-circle-find': function () {
+    'touchstart #x-circle-find': function (event, template) {
       document.getElementById('peek-search').style.display="none" 
       document.getElementById('peek').style.display="block" 
       document.getElementById('logo-corner').style.display="block"
       document.getElementById('full-search').style.display="none"
     },
     //hide full search view
-    'click #peek': function () {
+    'touchstart #peek': function (event, template) {
       document.getElementById('main-nav').style.display="block" 
       document.getElementById('peek').style.display="none"
       document.getElementById('logo-corner').style.display="block"      
     },
     //WUT WUT INFO/about PAGE
-    'click #about-icon-btn': function () {
+    'touchstart #about-icon-btn': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('main-nav').style.display="none"
       document.getElementById('about').style.display="block"
-      document.getElementById('logo-corner').style.display="none"  
+      document.getElementById('logo-corner').style.display="block"  
     },
     //BYE BYE INFO/about PAGE
-    'click #about': function () {
+    'touchstart #about': function (event, template) {
       document.getElementById('muro-welcome').style.display="block"
       document.getElementById('main-nav').style.display="block"
       document.getElementById('about').style.display="none"
       document.getElementById('logo-corner').style.display="none" 
     },
-    'click #create-wall-form-backer': function () {
+    'touchstart #create-wall-form-backer': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('create-wall-form').style.display="none"
       document.getElementById('create-wall-form-backer').style.display="none"
       document.getElementById('logo-corner').style.display="block"
     },
-    'click #back-btn': function () {
+    'touchstart #back-btn': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('art-wall-backer').style.display="none"
       document.getElementById('art-wall').style.display="none" 
       document.getElementById('logo-corner').style.display="block" 
   
     },
-    'click #art-wall-backer': function () {
+    'touchstart #art-wall-backer': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('art-wall-backer').style.display="none"
       document.getElementById('art-wall').style.display="none"
       document.getElementById('logo-corner').style.display="block" 
     },
-    'click #add-btn': function () {
+    'touchstart #add-btn': function (event, template) {
       document.getElementById('muro-welcome').style.display="none"
       document.getElementById('art-wall-backer').style.display="none"
       document.getElementById('art-wall').style.display="none"  
